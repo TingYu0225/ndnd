e2c9380d981811d9ecefd7dd2bc3e854f6df2ea4
tingyu <0225natasha@gmail.com>
2026-03-06 00:59:56 -0800

    add publish insertion file

--- repo/insertion.go
+++ repo/insertion.go
@@ -3,11 +3,13 @@ package repo
 import (
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/named-data/ndnd/repo/tlv"
 	enc "github.com/named-data/ndnd/std/encoding"
 	"github.com/named-data/ndnd/std/engine"
+	"github.com/named-data/ndnd/std/ndn"
 	spec "github.com/named-data/ndnd/std/ndn/spec_2022"
 	"github.com/named-data/ndnd/std/object"
 	"github.com/named-data/ndnd/std/object/storage"
@@ -18,7 +20,7 @@ type RepoCommandParam struct {
 	file_path       string   // Input file path, or - for stdin
 	file_name       enc.Name // Data packet name, or a name prefix of segmented Data packets.
 	start_block_id  uint64   // Start block ID for segmented data(interest)
-	end_id          uint64
+	end_block_id    uint64
 	forwarding_hint enc.Name
 }
 
@@ -27,7 +29,7 @@ func CmdRepoInsert() *cobra.Command {
 		file_path:       "",
 		file_name:       enc.Name{}, // Data packet name, or a name prefix of segmented Data packets.
 		start_block_id:  0,          // Start block ID for segmented data(interest)
-		end_id:          0,
+		end_block_id:    0,
 		forwarding_hint: enc.Name{},
 	}
 
@@ -71,6 +73,10 @@ func checkInsertRequest(t *RepoCommandParam, filePath string, packetNameStr stri
 	return true
 }
 
+// func insert(name enc.Name, start_block_id uint64, end_block_id uint64, client ndn.Client) error {
+
+// }
+
 func (t *RepoCommandParam) run(_ *cobra.Command, args []string) {
 	filePath := args[0]
 	fileName := args[1]
@@ -93,6 +99,7 @@ func (t *RepoCommandParam) run(_ *cobra.Command, args []string) {
 	defer app.Stop()
 
 	// Create a client
+	// TODO: use trust config
 	client := object.NewClient(app, storage.NewMemoryStore(), nil)
 	if err := client.Start(); err != nil {
 		fmt.Println("client start failed:", err)
@@ -100,18 +107,64 @@ func (t *RepoCommandParam) run(_ *cobra.Command, args []string) {
 	}
 	defer client.Stop()
 
-	// // Read file content
-	// content, err := os.ReadFile(filePath)
-	// if err != nil {
-	// 	fmt.Println("failed to read file:", err)
-	// 	return
-	// }
+	// Read file content and chunk if necessary
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("failed to read file:", err)
+		return
+	}
+	vname, err := client.Produce(ndn.ProduceArgs{
+		Name:    t.file_name.WithVersion(enc.VersionUnixMicro),
+		Content: enc.Wire{content},
+	})
+	if err != nil {
+		fmt.Println("failed to produce:", err)
+		return
+	}
+	// calculate end_block_id based on content size (Produce will chunk the content into segments of size 8000)
+	lastSeg := uint64(0)
+	if len(content) > 0 {
+		lastSeg = uint64((len(content) - 1) / 8000)
+	}
+	t.end_block_id = lastSeg
 
-	// // Produce the data so repo can fetch it
-	// client.Produce(ndn.ProduceArgs{
-	// 	Prefix:  t.file_name,
-	// 	Content: content,
-	// })
+	fmt.Printf("Produced Data with name: %s, segments: 0-%d\n", vname.String(), lastSeg)
+
+	// Announce prefix before BlobFetch so server can fetch segments.
+	client.AnnouncePrefix(ndn.Announcement{
+		Name:   vname,
+		Expose: true,
+	})
+	defer client.WithdrawPrefix(vname, nil) // Withdraw prefix after command is done or timeout, so server won't fetch other data with the same prefix.
+
+	announceStop := make(chan struct{}) // notify background goroutine to stop announcing when BlobFetch is done or timeout.
+	announceDone := make(chan struct{}) // Stop announcement when BlobFetch is done or timeout.
+	var stopOnce sync.Once
+	stopAnnounce := func() {
+		stopOnce.Do(func() {
+			close(announceStop)
+			<-announceDone // block until goroutine stop announceDone
+		})
+	}
+	defer stopAnnounce()
+
+	// Keep announcement alive in background while server is fetching.
+	go func() {
+		ticker := time.NewTicker(2 * time.Second)
+		defer ticker.Stop()
+		defer close(announceDone)
+		for {
+			select {
+			case <-announceStop:
+				return
+			case <-ticker.C:
+				client.AnnouncePrefix(ndn.Announcement{
+					Name:   vname,
+					Expose: true,
+				})
+			}
+		}
+	}()
 
 	// Create command name
 	cmdName := t.file_name.
@@ -122,11 +175,13 @@ func (t *RepoCommandParam) run(_ *cobra.Command, args []string) {
 	payload := (&tlv.RepoCmd{
 		BlobFetch: &tlv.BlobFetch{
 			Name: &spec.NameContainer{
-				Name: t.file_name,
+				Name: vname,
 			},
+			Data: [][]byte{[]byte("insert")},
 		},
 	}).Encode()
 
+	println("send BlobFetch command")
 	// Send command to repo
 	done := make(chan error, 1)
 	client.ExpressCommand(t.forwarding_hint, cmdName, payload, func(w enc.Wire, err error) {
@@ -150,12 +205,14 @@ func (t *RepoCommandParam) run(_ *cobra.Command, args []string) {
 	// Wait for response or timeout
 	select {
 	case err := <-done:
+		stopAnnounce()
 		if err != nil {
 			fmt.Println("failed:", err)
 			return
 		}
 		fmt.Println("insert command success")
-	case <-time.After(4 * time.Second):
+	case <-time.After(180 * time.Second):
+		stopAnnounce()
 		fmt.Println("insert command timeout")
 	}
 }
